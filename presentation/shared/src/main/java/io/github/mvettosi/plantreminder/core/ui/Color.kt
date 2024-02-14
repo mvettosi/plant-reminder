8d6dddd728d89a20c99fbaa3af349eab69643971
matteovettosi <matteo.vettosi@gmail.com>
2024-02-14 22:09:29 +0000

    Added colours from Figma

--- presentation/shared/src/main/java/io/github/mvettosi/plantreminder/core/ui/Color.kt
+++ presentation/shared/src/main/java/io/github/mvettosi/plantreminder/core/ui/Color.kt
@@ -18,6 +18,31 @@ package io.github.mvettosi.plantreminder.core.ui
 
 import androidx.compose.ui.graphics.Color
 
+// Neutralus
+val Neutralus900 = Color(0xFF232926)
+val Neutralus500 = Color(0xFF516370)
+val Neutralus300 = Color(0xFFAFB3B7)
+val Neutralus100 = Color(0xFFF9F9F9)
+val Neutralus0 = Color(0xFFFFFFFF)
+
+// Accent
+val Accent600 = Color(0xFF085D6F)
+val Accent500 = Color(0xFF0A6375)
+val Accent100 = Color(0xFF8CB6B2)
+
+// Other
+val OtherG500 = Color(0xFFCFE3C4)
+val OtherG100 = Color(0xFFF5F9F3)
+val OtherP500 = Color(0xFFFAD2E2)
+val OtherP100 = Color(0xFFFFFAFC)
+val OtherL500 = Color(0xFFB2D99E)
+val OtherL100 = Color(0xFFF9FBF8)
+val OtherB500 = Color(0xFFA7D1E2)
+val OtherB100 = Color(0xFFF5FAFC)
+val OtherB2500 = Color(0xFFDBE1B8)
+val OtherB2100 = Color(0xFFFDFFF1)
+
+//TODO delete these
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
